b45b63c4a67b3777ea5f5d709f6f6f804dad9ddb
jeclique444 <jericlique24@gmail.com>
2026-07-18 13:22:49 +0800

    GT2 Part 1: Add vite-env.d.ts to fix CSS import

---
+++ src/vite-env.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
